bf7ef87df333b6fa12616a430d422aa8c75c6432
Mr.mobin <barmar.mobin@gmail.com>
2022-09-23 23:19:36 +0330

    practice1 ended

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
+const path = require('path');
+
 const express = require('express')
 const bodyParser = require('body-parser');
+const rootDir = require('./utils/path');
 
 const adminRoutes = require('./routes/admin');
 const homeRoutes = require('./routes/home');
+const { dirname } = require('path');
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(rootDir, 'public')))
 
 //? Routes
 app.use('/admin', adminRoutes)
 app.use(homeRoutes)
 //! End Routes
 
-
+app.use('/', (req, res) => {
+    res.sendFile(path.join(rootDir, 'views', '404.html'))
+})
 
 
 
--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,16 @@
 const path = require('path');
 
 const express = require('express');
+const rootDir = require('../utils/path');
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname ,'../views', 'index.html') )
+    res.sendFile(path.join(rootDir ,'views', 'index.html') )
 })
 
 router.get('/hello', (req, res) => {
-    res.send('<h1>Hello Mobin!</h1>')
+    res.sendFile(path.join(rootDir, 'views', 'hello.html'))
 })
 
 module.exports = router
